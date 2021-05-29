8cb340b9720d5cb510f6e703ab746470f434c954
ryuparish <ryuparish1115@gmail.com>
2021-05-29 15:01:26 -0500

    Very messy homogenenous linked list in python

--- data_structures/elementary/homogen_linked.py
+++ data_structures/elementary/homogen_linked.py
@@ -1,10 +1,6 @@
 free = 0
-# REMEMBER, YOU NEED TO FIX THE CODE SO THAT WHEN YOU FREE AN INDEX, IT FREES THE INDEX IN THE MASTER LIST AND ALLOCATES IT IN THE FREE_LIST
-# THE FIRST INDEX TO BE DEALLOCATED IS THE INDEX THAT IS GIVEN, NOT THE LAST INDEX THAT WAS FREED. THAT WILL CONFLICT WITH THE CODE SHOWN IN THE BOOK (FOR DEALLOCATE)
-# We first need to initialize the free list
 class free_list:
     def __init__(self, size):
-        
         # The free list will contain the index to be allocated (the index itself) and the
         # location of the next index that is the be the free "head"
         self.free_links = [x+1 for x in range(size-1)]
@@ -16,7 +12,6 @@ class free_list:
         # Giving the allocated index to x and setting the allocated spot to
         # None
         x = free
-        print("Value of free in free_list allocate: ", free)
 
         # If there are no more spaces to be allocated on the free list 
         if(self.free_links[free] == None):
@@ -36,7 +31,6 @@ class free_list:
         global free
         if(self.free_links[x] != None):
             raise ValueError("The value to be freed is already freed.")
-        print("Value of free in deallocate: ", free)
         self.free_links[x] = free
         free = x
 
@@ -61,22 +55,16 @@ class homogen_list:
 
     # Use index function instead of directly indexing master_list
     def index(self, index):
-        #if(self.master_list[index * 3] == None):
-        #    print("This index,", index, "is not allocated. Returning None.")
-        #    return None
         return index * 3
 
-    # Use next function instead of directly accessing the index and adding 1
     def next_index(self, index):
         return self.index(index) + 1
 
-    # Use prev function instead of directly accessing the index and adding 2
     def prev_index(self, index):
         return self.index(index) + 2
 
     def allocate_object(self, element):
         x = self.free_memory.allocate()
-        print("Free index given: ", x)
 
         # Setting the spot allocated's value to the item placed in the list
         self.master_list[self.index(x)] = element
@@ -95,17 +83,17 @@ class homogen_list:
         # Allocating back to the free list
         self.free_memory.deallocate(index)
 
-        # Saving the prev index for travel then empyting out the deallocated spot 
-        former_prev = self.master_list[self.prev_index(self.head)]
-        self.master_list[self.prev_index(self.head)] = None
-        self.master_list[self.next_index(self.head)] = None
-        self.master_list[self.index(self.head)] = None
-        self.head = former_prev
 
-        # Now making the next value of the prev spot equal to None since it has been deallocated
-        self.master_list[self.next_index(self.head)] = None
+        # Empty the indexed link and set the previous' next to the next and the next's prev to the previous
+        prev_head = self.prev_index(self.head)
+        self.master_list[self.index(index)] = None
+        self.master_list[self.prev_index(self.master_list[self.next_index(index)])] = self.master_list[self.prev_index(index)]
+        self.master_list[self.next_index(self.master_list[self.prev_index(index)])] = self.master_list[self.next_index(index)]
+        self.master_list[self.index(index)] = None
+        self.master_list[self.prev_index(index)] = None
+        self.master_list[self.next_index(index)] = None
 
-# Print out length of linked list
+
 def print_list(linked_List):
     x = 0
     print("Here are the values within the linked list: ")
@@ -114,7 +102,6 @@ def print_list(linked_List):
         print(value)
         x = linked_List.master_list[linked_List.next_index(x)]
 
-# Driver function
 def main():
     # The value of starter must be changed every time something is freed
     # added to the linked list
@@ -122,53 +109,35 @@ def main():
 
     # Allocating one object to the linked list and printing out the free list and the master list between each allocation
     linked_list.allocate_object(5)
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
     print_list(linked_list)
+
     linked_list.allocate_object(6)
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
     print_list(linked_list)
+
     linked_list.allocate_object(7)
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
     print_list(linked_list)
+
     linked_list.allocate_object(8)
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
     print_list(linked_list)
+
     linked_list.allocate_object(9)
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
     print_list(linked_list)
+
     linked_list.allocate_object(10)
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
     print_list(linked_list)
+
     linked_list.allocate_object(11)
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
     print_list(linked_list)
+
     linked_list.allocate_object(12)
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
-
-    # Print out the homogen list
     print_list(linked_list)
 
-    # Print out after deallocating
     linked_list.deallocate_object(2)
     print("Just deallocated")
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
     print_list(linked_list)
 
-    # Print out the homogen list after allocating again
     linked_list.allocate_object(7)
     print("Just allocated")
-    print(linked_list.master_list)
-    print(linked_list.free_memory.free_links)
-    exit(0)
     print_list(linked_list)
 
 
