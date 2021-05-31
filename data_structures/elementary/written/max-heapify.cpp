1830ed273204fec36eabeffb4fff17cd45e02ee4
ryuparish <ryuparish1115@gmail.com>
2021-05-30 19:46:30 -0500

    Added some written answers that are not so great

---
+++ data_structures/elementary/written/max-heapify.cpp
@@ -0,0 +1,16 @@
+// Just Pseudo code please do not crucify me
+for(int i = i; i < A.heap-size; ++i){
+    int largest;
+    int l = i.left;
+    int r - i.right;
+    if (A[l] > A[i]):
+        largest = l;
+    else{largest = i;}
+    if (A[r] > A[largest]):
+        largest = r;
+    if largest != i:
+        exchange A[i] with A[largest];
+}
+
+
+
